9659880e898842bf3bc7e7f7f371d5e69cfef57c
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2022-07-06 22:02:01 +0600

    Chapter 1

---
+++ C1.1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+printf("..................\n");
+printf("First line : Fahim\nSecond Line : 12/a\nThird Line : Dhaka 1207\n");
+printf("..................");
+getch();
+}
